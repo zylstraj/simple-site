95e6cb26d2ae93f7b86ff36e947a0dcd85d0d122
Mel Quark <mel.quark@gmail.com>
2014-12-21 17:32:09 -0800

    oops

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ var quotes = [
   "Life can be so sweet on the sunny side of the street"
 ];
 
+var jokes = [
+  { setup: "What did the Zen buddhist say to the hotdog vendor?", 
+    punchline: "Make me one with everything."},
+  { setup: "What did the mushroom say when asked what she looked for in a date?",
+    punchline: "A fun guy."},
+  { setup: "How many tickles does it take to make an octupus laugh?",
+    punchline: "Ten Tickles"}
+];
+
+app.get("/joke", function (req, res){
+  var randomIndex = Math.floor(Math.random()*jokes.length);
+  res.json(jokes[randomIndex]);
+});
+
 app.get("/inspiration", function (req, res){
   var randomIndex = Math.floor(Math.random()*quotes.length);
   res.send(quotes[randomIndex]);
-});
-app.get("/joke", function (req, res){
-  res.send("beep");
 });
 
 app.get("/", function (req, res){
