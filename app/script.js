cdf341f7cae0e3db1c16d3eda68ec2dfbb31b075
Josh Zylstra <joshzylstra@Joshs-MacBook-Pro.local>
2015-03-17 10:23:21 -0700

    random generator server

--- app/script.js
+++ app/script.js
@@ -1,10 +1,4 @@
-/*$(document).ready(function(){
-	
-	function randomString() {
-
-var information = ["Do Better", "Try Harder", "You Got It", "Dont Stop Yet", "Awesome"];*/
-
-var information	 = ["Do Better", "Try Harder", "You Got It", "Dont Stop Yet", "Awesome"];
+var information	 = [ "Do Better", "Try Harder", "You Got It", "Dont Stop Yet", "Awesome" ];
 
 function randomString(item) {
 var randomIndex = item[Math.floor(Math.random() * item.length)];
@@ -27,22 +21,22 @@ return words[Math.floor(Math.random() * words.length)];
 		});
 	});
 });*/
-$(function(){
-	$(document).ready(function(){
-		$.get("http://localhost:3000/favorite", function(response){
+$(function() {
+	$(document).ready(function() {
+		$.get("http://localhost:3000/favorite", function(response) {
 			$("#ajax-learning").text(response.favoritestadium + ": " + response.reasoning);
 			console.log(response);
 		});
 	});
 });
 
-$(function(){
-	$("button").click(function(){
+$(function() {
+	$("button").click(function() {
 		var url = $(this).attr("id");
-		$.get(url, function(response){
+		$.get(url, function(response) {
 			var resText;
 			console.log(response);
-			if(typeof response === "object"){
+			if (typeof response === "object") {
 				resText = response.setup + ": " + response.punchline;
 			} else {
 				resText = response;
@@ -53,102 +47,29 @@ $(function(){
 	});
 });
 
-/* $.get("http://localhost:3000/quote", function(response){
-$("#ajax-text").text(response);
-});
-/*$(function() {
-	$("button").click(function() {
-		$.get("http://localhost:3000/stadium", function(response){
-			var resText;
-			if (typeof response === "object") {
-				resText = response.setup + ": " + response.punchline;
-			} else {
-				resText = response	;
-			}
-			$("#ajax-text").text(resText);
-	});
-});*/
-
-/*$(function() {
-$("button").click(function() {
-var url = $(this)attr("id");
-
-$.get(url, function(response) { 
-	var resText; 
-	if (typeof response === "object") {
-		resText = response.favoritestadium + ": " + response.reasoning;
-	} else {
-		resText = response;
-	}
-	console.log(response);
-	$("#ajax-text").text(response.favoritestadium + ": " + response.reasoning);
-	}); 
-	});
-});*/
-/*$(function() {
-	$('button').click(function(){
-		$.get("http://localhost:3000/stadium", function(response){
-		$("#ajax-text").text(response.favoritestadium + ": " + response.reasoning);
-	});
-});
-});*/
-
-/*$(function(){
-		$("button").click(function(){
-			$.get("http://localhost:3000/quote", function(response){
-				$("ajax-text").text(response);
-			});
-		});
-});*/
-/*$("#piglatin").on("submit", function(e) {
-	e.preventDefault();
-	var firstname = $('input[name=firstname]').val();
-	var lastname = $('input[name=lastname]').val();
-	var name = {firstname: firstname, lastname: lastname};
-	console.log(name);
-	$.post("piglatin", name, function(response) {
-var piglatinified = response.firstname + " " +
-response.lastname; $("#piglatinified").text(piglatinified);
-});
-});*/
-
 $("#piglatin").on("submit", function(e) {
 	e.preventDefault();
-	var firstname = $('input[name=firstname]').val();
-	var lastname = $('input[name=lastname]').val();
-	var name = {firstname: firstname, lastname: lastname};
+	var firstname = $("input[name=firstname]").val();
+	var lastname = $("input[name=lastname]").val();
+	var name = { firstname: firstname, lastname: lastname };
 	console.log(name);
 	$.post("piglatin", name, function(response) {
 		var piglatinified = response.firstname + " " +
-response.lastname; 
+response.lastname;
 		$("#piglatinified").text(piglatinified);
 	});
 });
 
 $("#basic").on("submit", function(e) {
 	e.preventDefault();
-	var first = $('input[name=first]').val();
-	var last = $('input[name=last]').val();
-	var birthcity = $('input[name=birthcity]').val();
-	var something = {first: first, last: last, birthcity: birthcity};
+	var first = $("input[name=first]").val();
+	var last = $("input[name=last]").val();
+	var birthcity = $("input[name=birthcity]").val();
+	var something = { first: first, last: last, birthcity: birthcity };
 	console.log(something);
 	$.post("basic", something, function(response) {
 var basicified = response.first + " " +
-response.last + " from " + response.birthcity; 
+response.last + " from " + response.birthcity;
 $("#basicinformation").text(basicified);
 });
 });
-
-/*$("#basic").on("submit", function(e) {
-	e.preventDefault();
-	var firstname = $('input[name=firstname]').val();
-	var lastname = $('input[name=lastname]').val();
-	var birthcity = $('input[name = birthcity]').val();
-	var basicinfo = {firstname: firstname, lastname: lastname, birthcity: birthcity};
-	console.log(basicinfo);
-
-	$.post("basic", basicinfo, function(response) {
-		var whoyouare = response.firstname + " " + response.lastname + " from " + response.birthcity;
-		$("#basicinformation").text(whoyouare);
-	});
-});*/
--- server.js
+++ server.js
@@ -9,10 +9,10 @@ var piglatinify = require("./lib/piglatinify.js");
 var basicYou = require("./lib/basicinfo.js");
 var quotes = require("./lib/quotes.js");
 var jokes = require("./lib/jokes.js");
-var favorite = require("./lib/favorite.js");
+//var favorite = require("./lib/favorite.js");
 //var maybe = require("./lib/quotes.js");
-app.use(bodyparser.json()); 
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //app.use(express.static(__dirname + "/app/"));
 
@@ -20,99 +20,46 @@ app.use(bodyparser.urlencoded({extended: true}));
 */
 app.use(express.static(__dirname + "/app/"));
 
-/*app.get("/", function(req, res){
-	res.sendFile("index.html");
-});
-
-function randomString(words) {
-
-return words[Math.floor(Math.random() * words.length)];
-}
-app.get("/information", function(req, res){s
-	var todaysLearning = randomString(information);
-	res.send(todaysLearning);
-});*/
-//var quotes = ["Try Hard", "Be True To You", "Love", "Generosity"];
-
-/* var jokes = [
-{ setup: "What's the difference between a guitar and a fish?", 
-punchline: "you can't tuna fish."}, 
-{setup: "Whats black and blue?",
-punchline: "Your Mom!"},
-{setup: "Why did no one hear Helen Keller fall off the mountain?",
-punchline: "Because she had mittens on."}
-];*/
-
 var favorite = [
 { favoritestadium: "Wrigley Field",
-reasoning: "The ivy is gorgeous and nothing beats the bleachers in the middle of summer!"},
+reasoning: "The ivy is gorgeous and nothing beats the bleachers in the middle of summer!" },
 { favoritestadium: "Fenway Park",
-reasoning: "The green monster"},
-{ favoritestadium : "Dodger Stadium",
-reasoning: "Los Angeles can not be beat. City baseball"}
-]
+reasoning: "The green monster" },
+{ favoritestadium: "Dodger Stadium",
+reasoning: "Los Angeles can not be beat. City baseball" }
+];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 	res.sendFile("index.html");
 });
 
-app.get("/quote", function(req, res){
-	var randomIndex = Math.floor(Math.random()*quotes.length);
+app.get("/quote", function(req, res) {
+	var randomIndex = Math.floor(Math.random() * quotes.length);
 	res.send(quotes[randomIndex]);
 });
 //app.get("/quote", maybe);
 
-app.get("/joke", function(req, res){
+app.get("/joke", function(req, res) {
 	var randomIndex = Math.floor(Math.random() * jokes.length);
 	res.json(jokes[randomIndex]);
 });
 
-app.get("/favorite", function(req, res){
+app.get("/favorite", function(req, res) {
 	var randomIndex = Math.floor(Math.random() * favorite.length);
 	res.json(favorite[randomIndex]);
 });
 
 app.listen(port, function() {
-	console.log('server started on port ' + port);
+	console.log("server started on port " + port);
 });
 //starts server on local machine at port 3000 - this means
 // to talk to the server - use this address
 
-/*var quotes = ["Do Better", "Try Harder", "You Got It", "Dont Stop Yet", "Awesome"];
-
-app.get("/quote", function (req, res){
-var randomIndex = Math.floor(Math.random()*quotes.length);
-res.send(quotes[randomIndex]); });*/
-
-
-/*function piglatinify(word) {
-	var wordArray = word.split("");
-	var letters;
-	var changedWord;
-	var vowelHash = {a: 1, e: 1, i: 1, o:1, u:1, A: 1, E: 1, I: 1, O: 1, U: 1};
-
-	if (vowelHash.hasOwnProperty(wordArray[0])) { 
-		return word + "-hay";
-}
-	letters = wordArray.shift();
-	changedWord = wordArray.join("") + "-" + letters + "ay";
-	return changedWord;
-
-}*/
-
-/*function basicYou(info) {
-	//var fun = info.split("");
-	//var funtimes;
-	console.log(info);
-	return info.toUpperCase();
-	//return info.charAt(0).toUpperCase() + info.substring(1);
-}*/
-
 app.post("/piglatin", function(req, res) {
-var firstname = piglatinify(req.body.firstname); 
-var lastname = piglatinify(req.body.lastname); 
-var piglatined = {firstname: firstname, lastname:
-lastname}; 
+var firstname = piglatinify(req.body.firstname);
+var lastname = piglatinify(req.body.lastname);
+var piglatined = { firstname: firstname, lastname:
+lastname };
 res.json(piglatined);
 });
 
@@ -120,39 +67,6 @@ app.post("/basic", function(req, res) {
 	var first = basicYou(req.body.first);
 	var last = basicYou(req.body.last);
 	var birthcity = basicYou(req.body.birthcity);
-	var basicinfo = {first: first, last: last, birthcity: birthcity};
+	var basicinfo = { first: first, last: last, birthcity: birthcity };
 	res.json(basicinfo);
-})
-
-/*var favorite = [
-{ favoritestadium: "Wrigley Field",
-reasoning: "The ivy is gorgeous and nothing beats the bleachers in the middle of summer!"},
-{ favoritestadium: "Fenway Park",
-reasoning: "The green monster"},
-{ favoritestadium : "Dodger Stadium",
-reasoning: "Los Angeles can not be beat. City baseball"}
-]*/
-
-/*This part of the code simply creates the array containing 3 objects
-QUESTION - (Am I supposed to create this as a JSON object - - or did something like
-	//this work and we we doing simliar thing to jokes?*/
-
-/*app.get("/stadium", function(req, res){
-	var randomIndex = Math.floor(Math.random() * favorite.length);
-	res.json(favorite[randomIndex]);
-	});*/
-	/*the first argument of app.get creates the endpoint stadium
-	when this first argument is trigger, 
-	it activates the second function */
-
-/*this creates a random index based off the length of the 
- JS object favorite */
-
-/*res responds by sending by a JSON object of the JS object favorite
-but it only sends the index which was created/access by the previous 
-line of code*/
-
-
-/*yesyes = randomString(jokes);
-	res.send(yesyes);
-}); */
+});
