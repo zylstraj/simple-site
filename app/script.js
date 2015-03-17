17cff0c9f8cb3e8bf1fef2e794eaf37a98234dfb
Josh Zylstra <joshzylstra@Joshs-MacBook-Pro.local>
2015-03-17 09:32:09 -0700

    randomness generator server

--- app/script.js
+++ app/script.js
@@ -28,10 +28,20 @@ return words[Math.floor(Math.random() * words.length)];
 	});
 });*/
 $(function(){
+	$(document).ready(function(){
+		$.get("http://localhost:3000/favorite", function(response){
+			$("#ajax-learning").text(response.favoritestadium + ": " + response.reasoning);
+			console.log(response);
+		});
+	});
+});
+
+$(function(){
 	$("button").click(function(){
 		var url = $(this).attr("id");
 		$.get(url, function(response){
 			var resText;
+			console.log(response);
 			if(typeof response === "object"){
 				resText = response.setup + ": " + response.punchline;
 			} else {
@@ -95,11 +105,50 @@ $.get(url, function(response) {
 	var firstname = $('input[name=firstname]').val();
 	var lastname = $('input[name=lastname]').val();
 	var name = {firstname: firstname, lastname: lastname};
-
-
+	console.log(name);
 	$.post("piglatin", name, function(response) {
 var piglatinified = response.firstname + " " +
 response.lastname; $("#piglatinified").text(piglatinified);
 });
-})*/
+});*/
 
+$("#piglatin").on("submit", function(e) {
+	e.preventDefault();
+	var firstname = $('input[name=firstname]').val();
+	var lastname = $('input[name=lastname]').val();
+	var name = {firstname: firstname, lastname: lastname};
+	console.log(name);
+	$.post("piglatin", name, function(response) {
+		var piglatinified = response.firstname + " " +
+response.lastname; 
+		$("#piglatinified").text(piglatinified);
+	});
+});
+
+$("#basic").on("submit", function(e) {
+	e.preventDefault();
+	var first = $('input[name=first]').val();
+	var last = $('input[name=last]').val();
+	var birthcity = $('input[name=birthcity]').val();
+	var something = {first: first, last: last, birthcity: birthcity};
+	console.log(something);
+	$.post("basic", something, function(response) {
+var basicified = response.first + " " +
+response.last + " from " + response.birthcity; 
+$("#basicinformation").text(basicified);
+});
+});
+
+/*$("#basic").on("submit", function(e) {
+	e.preventDefault();
+	var firstname = $('input[name=firstname]').val();
+	var lastname = $('input[name=lastname]').val();
+	var birthcity = $('input[name = birthcity]').val();
+	var basicinfo = {firstname: firstname, lastname: lastname, birthcity: birthcity};
+	console.log(basicinfo);
+
+	$.post("basic", basicinfo, function(response) {
+		var whoyouare = response.firstname + " " + response.lastname + " from " + response.birthcity;
+		$("#basicinformation").text(whoyouare);
+	});
+});*/
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var app = express();
-//var bodyparser = require("body-parser");
+var bodyparser = require("body-parser");
 var port = process.env.PORT || 3000; //listen to requests
 // on port 3000 - that is what is going on here
 // you can set port to something else but 3000 is standard
 
-/*var piglatinify = require("./lib/piglatinify.js");
-
+var piglatinify = require("./lib/piglatinify.js");
+var basicYou = require("./lib/basicinfo.js");
+var quotes = require("./lib/quotes.js");
+var jokes = require("./lib/jokes.js");
+var favorite = require("./lib/favorite.js");
+//var maybe = require("./lib/quotes.js");
 app.use(bodyparser.json()); 
-app.use(bodyparser.urlencoded({extended: true}));*/
+app.use(bodyparser.urlencoded({extended: true}));
 
 //app.use(express.static(__dirname + "/app/"));
 
@@ -28,16 +32,25 @@ app.get("/information", function(req, res){s
 	var todaysLearning = randomString(information);
 	res.send(todaysLearning);
 });*/
-var quotes = ["Try Hard", "Be True To You", "Love", "Generosity"];
+//var quotes = ["Try Hard", "Be True To You", "Love", "Generosity"];
 
-var jokes = [
+/* var jokes = [
 { setup: "What's the difference between a guitar and a fish?", 
 punchline: "you can't tuna fish."}, 
 {setup: "Whats black and blue?",
 punchline: "Your Mom!"},
 {setup: "Why did no one hear Helen Keller fall off the mountain?",
 punchline: "Because she had mittens on."}
-];
+];*/
+
+var favorite = [
+{ favoritestadium: "Wrigley Field",
+reasoning: "The ivy is gorgeous and nothing beats the bleachers in the middle of summer!"},
+{ favoritestadium: "Fenway Park",
+reasoning: "The green monster"},
+{ favoritestadium : "Dodger Stadium",
+reasoning: "Los Angeles can not be beat. City baseball"}
+]
 
 app.get("/", function(req, res){
 	res.sendFile("index.html");
@@ -47,10 +60,16 @@ app.get("/quote", function(req, res){
 	var randomIndex = Math.floor(Math.random()*quotes.length);
 	res.send(quotes[randomIndex]);
 });
+//app.get("/quote", maybe);
 
 app.get("/joke", function(req, res){
 	var randomIndex = Math.floor(Math.random() * jokes.length);
 	res.json(jokes[randomIndex]);
+});
+
+app.get("/favorite", function(req, res){
+	var randomIndex = Math.floor(Math.random() * favorite.length);
+	res.json(favorite[randomIndex]);
 });
 
 app.listen(port, function() {
@@ -79,15 +98,31 @@ res.send(quotes[randomIndex]); });*/
 	changedWord = wordArray.join("") + "-" + letters + "ay";
 	return changedWord;
 
-}
+}*/
+
+/*function basicYou(info) {
+	//var fun = info.split("");
+	//var funtimes;
+	console.log(info);
+	return info.toUpperCase();
+	//return info.charAt(0).toUpperCase() + info.substring(1);
+}*/
 
 app.post("/piglatin", function(req, res) {
 var firstname = piglatinify(req.body.firstname); 
 var lastname = piglatinify(req.body.lastname); 
 var piglatined = {firstname: firstname, lastname:
-lastname}; res.json(piglatined);
-});*/
+lastname}; 
+res.json(piglatined);
+});
 
+app.post("/basic", function(req, res) {
+	var first = basicYou(req.body.first);
+	var last = basicYou(req.body.last);
+	var birthcity = basicYou(req.body.birthcity);
+	var basicinfo = {first: first, last: last, birthcity: birthcity};
+	res.json(basicinfo);
+})
 
 /*var favorite = [
 { favoritestadium: "Wrigley Field",
